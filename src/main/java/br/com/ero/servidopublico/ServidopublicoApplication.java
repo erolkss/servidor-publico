460cb324a1387183d1fbf424efa313fba253d1ad
Lucas Lima <lucaserolima@gmail.com>
2024-02-24 21:21:32 -0400

    first commit

---
+++ src/main/java/br/com/ero/servidopublico/ServidopublicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.servidopublico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidopublicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidopublicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ero/servidopublico/ServidopublicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.servidopublico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidopublicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
