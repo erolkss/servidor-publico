9799f7641effab8aa46cb9db2a6b66ce999018da
Lucas Lima <lucaserolima@gmail.com>
2024-02-25 20:41:08 -0400

    Updating project due to a JDK 17 error I had to update to JDK 21

--- src/test/java/com/abctreinamentos/servidorpublico/ServidorpublicoApplicationTests.java
+++ src/test/java/com/abctreinamentos/servidorpublico/ServidorpublicoApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.ero.servidopublico;
+package com.abctreinamentos.servidorpublico;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServidopublicoApplicationTests {
+class ServidorpublicoApplicationTests {
 
 	@Test
 	void contextLoads() {
